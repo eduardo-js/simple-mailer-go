ccc6f1a609ffd0744b630dd8643ae9641fc9cc3a
eduardo-js <jose.eduardo.ds@gmail.com>
2023-03-14 21:50:45 -0300

    feat: db connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,57 @@
 package main
 
-import "os"
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
 
 var port = os.Getenv("PORT")
 
 func main() {
+	db := initDB()
+	db.Ping()
+}
 
+func initDB() *sql.DB {
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("can't connect to database")
+	}
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	for counts := 0; counts < 10; counts++ {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Failed to connect to DB, retrying in 1 second")
+			time.Sleep(1 * time.Second)
+			continue
+		} else {
+			log.Print("Connected to DB!")
+			return connection
+		}
+	}
+	return nil
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
