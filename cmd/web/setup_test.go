d08f3c4817e399b6a8587c867ddac09183191d0b
eduardo-js <jose.eduardo.ds@gmail.com>
2023-03-18 11:48:36 -0300

    chore: adding test setup

---
+++ cmd/web/setup_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"encoding/gob"
+	"log"
+	"net/http"
+	"os"
+	"simple-mailer-go/data"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+var testApp Config
+
+func TestMain(m *testing.M) {
+	gob.Register(data.User{})
+	gob.Register(data.User{})
+	session := scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	wait := &sync.WaitGroup{}
+	testApp = Config{
+		Session:   session,
+		DB:        nil,
+		InfoLog:   log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+		ErrorLog:  log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+		Wait:      wait,
+		ErrorChan: make(chan error),
+		DoneChan:  make(chan bool),
+		Mailer: Mail{
+			Wait:       wait,
+			DoneChan:   make(chan bool),
+			ErrorChan:  make(chan error),
+			MailerChan: make(chan Message, 100),
+		},
+	}
+
+	go func() {
+		select {
+		case <-testApp.Mailer.MailerChan:
+
+		case <-testApp.Mailer.ErrorChan:
+		case <-testApp.Mailer.DoneChan:
+			return
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case err := <-testApp.ErrorChan:
+				testApp.ErrorLog.Println(err)
+			case <-testApp.DoneChan:
+				return
+			}
+		}
+	}()
+
+	os.Exit(m.Run())
+}
+
+func getCtx(r *http.Request) context.Context {
+	ctx, err := testApp.Session.Load(r.Context(), r.Header.Get("X-Session"))
+	if err != nil {
+		log.Println(err)
+	}
+	return ctx
+}
